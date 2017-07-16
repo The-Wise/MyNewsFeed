dd851f4ae8ebfcdf3ce2cc8cb98910d3e95770f7
Daniel <didexe@gmail.com>
2017-07-16 18:56:32 +0300

    copy folder deleted

--- MyNewsFeed-App copy/main.js
+++
@@ -1,8 +0,0 @@
-const app = require('./config/app.js')(),
-  express = require('express');
-
-app.use('/static', express.static(__dirname + '/public'));
-
-app.listen(8080, () => {
-  console.log(`Server is running on port ${8080}.`);
-});
--- MyNewsFeed-App copy/routes/home-routes.js
+++
@@ -1,5 +0,0 @@
-const homeController = require('../controllers/home-controller.js')();
-
-module.exports = function (router) {
-  router.get('/', homeController.loadHomePage);
-};
